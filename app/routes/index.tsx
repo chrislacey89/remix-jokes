f4f83db82f2922ea23e8820d1d469e68e47cf38c
chrislacey89 <chrislacey89@gmail.com>
2021-12-29 20:26:59 -0500

    add background

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,10 @@
+import type { LinksFunction } from "remix";
+import stylesUrl from "../styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
 export default function IndexRoute() {
   return <div>Hello Index Route</div>;
 }
--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,31 @@
-export default function JokesIndexRoute() {
+import type { LinksFunction } from "remix";
+import { Link } from "remix";
+import stylesUrl from "../styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
+};
+
+export default function Index() {
   return (
-    <div>
-      <p>Here's a random joke:</p>
-      <p>I was wondering why the frisbee was getting bigger, then it hit me.</p>
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
