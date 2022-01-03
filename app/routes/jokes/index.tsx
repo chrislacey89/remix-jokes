6c42ba1f7a7de2ad81ecfc04a0d18de825cf0e08
chrislacey89 <chrislacey89@gmail.com>
2022-01-03 07:22:01 -0500

    add random joke

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,31 +1,29 @@
-import type { LinksFunction } from "remix";
-import { Link } from "remix";
-import stylesUrl from "../../styles/index.css";
+import type { LoaderFunction } from "remix";
+import { useLoaderData, Link } from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
 
-export const links: LinksFunction = () => {
-  return [
-    {
-      rel: "stylesheet",
-      href: stylesUrl,
-    },
-  ];
+type LoaderData = { randomJoke: Joke };
+
+export const loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  const data: LoaderData = { randomJoke };
+  return data;
 };
 
-export default function Index() {
+export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>();
+
   return (
-    <div className="container">
-      <div className="content">
-        <h1>
-          Remix <span>Jokes!</span>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="jokes">Read Jokes</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
     </div>
   );
 }
