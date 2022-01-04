c4397c4a7418fd6754b9d43d4270b2633499e636
chrislacey89 <chrislacey89@gmail.com>
2022-01-04 07:59:10 -0500

    send a cookie

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,5 +1,6 @@
 import type { ActionFunction, LinksFunction } from "remix";
 import { useActionData, json, Link, useSearchParams } from "remix";
+import { login, createUserSession } from "~/utils/session.server";
 import { db } from "~/utils/db.server";
 import stylesUrl from "../styles/login.css";
 
@@ -61,13 +62,14 @@ export const action: ActionFunction = async ({ request }) => {
 
   switch (loginType) {
     case "login": {
-      // login to get the user
-      // if there's no user, return the fields and a formError
-      // if there is a user, create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
+      const user = await login({ username, password });
+      if (!user) {
+        return {
+          fields,
+          formError: `Username/Password combination is incorrect`,
+        };
+      }
+      return createUserSession(user.id, redirectTo);
     }
     case "register": {
       const userExists = await db.user.findFirst({
