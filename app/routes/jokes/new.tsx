4809d5dfd8d1e6c6def1e757e71514e207c99d7e
chrislacey89 <chrislacey89@gmail.com>
2022-01-05 21:21:06 -0500

    prefetch

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,5 +1,5 @@
 import type { ActionFunction, LoaderFunction } from "remix";
-import { useActionData, redirect, json, useCatch, Link } from "remix";
+import { useActionData, redirect, json, useCatch, Link, Form } from "remix";
 import { db } from "~/utils/db.server";
 import { requireUserId, getUserId } from "~/utils/session.server";
 
@@ -69,7 +69,7 @@ export default function NewJokeRoute() {
   return (
     <div>
       <p>Add your own hilarious joke</p>
-      <form method="post">
+      <Form method="post">
         <div>
           <label>
             Name:{" "}
@@ -117,7 +117,7 @@ export default function NewJokeRoute() {
             Add
           </button>
         </div>
-      </form>
+      </Form>
     </div>
   );
 }
