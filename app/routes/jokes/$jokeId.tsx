a75f8e27bc1b131e2d5afad9296612c020fb8b68
chrislacey89 <chrislacey89@gmail.com>
2022-01-05 21:42:03 -0500

    add optemistic ui

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,15 +1,9 @@
-import type { ActionFunction, LoaderFunction, MetaFunction } from "remix";
-import {
-  Link,
-  useLoaderData,
-  useCatch,
-  redirect,
-  useParams,
-  Form,
-} from "remix";
+import type { LoaderFunction, ActionFunction, MetaFunction } from "remix";
+import { Link, useLoaderData, useCatch, redirect, useParams } from "remix";
 import type { Joke } from "@prisma/client";
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
+import { JokeDisplay } from "~/components/joke";
 
 export const meta: MetaFunction = ({
   data,
@@ -32,6 +26,7 @@ type LoaderData = { joke: Joke; isOwner: boolean };
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const userId = await getUserId(request);
+
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
   });
@@ -70,21 +65,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 export default function JokeRoute() {
   const data = useLoaderData<LoaderData>();
 
-  return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
-      {data.isOwner ? (
-        <Form method="post">
-          <input type="hidden" name="_method" value="delete" />
-          <button type="submit" className="button">
-            Delete
-          </button>
-        </Form>
-      ) : null}
-    </div>
-  );
+  return <JokeDisplay joke={data.joke} isOwner={data.isOwner} />;
 }
 
 export function CatchBoundary() {
@@ -111,7 +92,9 @@ export function CatchBoundary() {
   }
 }
 
-export function ErrorBoundary() {
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
   const { jokeId } = useParams();
   return (
     <div className="error-container">{`There was an error loading joke by the id ${jokeId}. Sorry.`}</div>
